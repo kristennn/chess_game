6b2fad1ca6a6fccfa52a9ddf5f4e45cdd233d927
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-11-26 03:38:57 +0800

    制作系统平台概况

--- app/controllers/tbl_systems_controller.rb
+++ app/controllers/tbl_systems_controller.rb
@@ -2,6 +2,12 @@ class TblSystemsController < ApplicationController
   layout 'admin'
   def index
     @tblaccounts = TblAccount.all
+    @tblorders = TblOrder.all
+    @tblplayers = TblPlayerinfo.all
     @online_count = 0
+    @active_count = 0
+    @signin_count = 0
+    @diamond_count = 0
+    @money_count = 0
   end
 end
