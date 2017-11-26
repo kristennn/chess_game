fe154d89f039a1160f05aebec74798cc5b99cd5f
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-11-26 22:37:04 +0800

    完成玩家用户的时间搜索

--- app/controllers/tbl_accounts_controller.rb
+++ app/controllers/tbl_accounts_controller.rb
@@ -9,6 +9,26 @@ class TblAccountsController < ApplicationController
       @tbl_accounts = @tbl_accounts.where( :userid => params[:userid].split(",") )
     end
 
+    if params[:create_time].present?
+      @tbl_accounts = @tbl_accounts.where( "create_time <= ?", Date.parse(params[:create_time]).end_of_day)
+      @tbl_accounts = @tbl_accounts.where( "create_time >= ?", Date.parse(params[:create_time]).beginning_of_day)
+    end
+
+    if params[:vip_invalid_time].present?
+      @tbl_accounts = @tbl_accounts.where( "vip_invalid_time <= ?", Date.parse(params[:vip_invalid_time]).end_of_day)
+      @tbl_accounts = @tbl_accounts.where( "vip_invalid_time >= ?", Date.parse(params[:vip_invalid_time]).beginning_of_day)
+    end
+
+    if params[:login_time].present?
+      @tbl_accounts = @tbl_accounts.where( "login_time <= ?", Date.parse(params[:login_time]).end_of_day)
+      @tbl_accounts = @tbl_accounts.where( "login_time >= ?", Date.parse(params[:login_time]).beginning_of_day)
+    end
+
+    if params[:logout_time].present?
+      @tbl_accounts = @tbl_accounts.where( "logout_time <= ?", Date.parse(params[:logout_time]).end_of_day)
+      @tbl_accounts = @tbl_accounts.where( "logout_time >= ?", Date.parse(params[:logout_time]).beginning_of_day)
+    end
+
   end
 
 end
