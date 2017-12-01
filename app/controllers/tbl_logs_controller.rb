848d198cfa212abdd7dd053da6ecb41f764fce91
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-12-01 17:11:46 +0800

    beauty css verson1

--- app/controllers/tbl_logs_controller.rb
+++
@@ -1,3 +0,0 @@
-class TblLogsController < ApplicationController
-  layout 'admin'
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :tbl_rooms
   resources :tbl_accounts
   resources :tbl_systems
-  resources :tbl_logs
+  resources :tbl_player_fees
 end
