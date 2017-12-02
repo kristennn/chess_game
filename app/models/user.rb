83b640c77306a15aecd14a5f36e02edea3905ba7
13949836783@163.com <13949836783@163.com>
2017-12-02 21:49:20 +0800

    浏览器记住登录cookie，但无法退出

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,16 +10,31 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  # 返回指定字符串的哈希摘要
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+
+    # 返回指定字符串的哈希摘要
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+
+    #返回一个随机令牌（用于remember_token）
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
-  #返回一个随机令牌（用于remember_token）
-  def User.new_token
-    SecureRandom.urlsafe_base64
+  #为了持久保持会话，在数据库中记住用户
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  #如果指定的令牌和摘要匹配，返回true
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
 end
