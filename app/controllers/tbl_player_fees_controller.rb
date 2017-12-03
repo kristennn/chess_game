91e51ca07247d2624cca4fc1524a90a236ec9594
13949836783@163.com <13949836783@163.com>
2017-12-03 15:21:02 +0800

    完成用户账号

--- app/controllers/tbl_player_fees_controller.rb
+++ app/controllers/tbl_player_fees_controller.rb
@@ -1,3 +1,8 @@
 class TblPlayerFeesController < ApplicationController
   layout 'admin'
+  def index
+    @playerfees = TblPlayerFee.all
+    @tblorders = TblOrder.all
+    @tblrooms = TblRoom.all
+  end
 end
