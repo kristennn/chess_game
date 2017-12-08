550bd492d933ff52a913ff8eeafd2a8cf86e2588
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-12-08 08:56:15 +0800

    ..

--- app/controllers/tbl_player_fees_controller.rb
+++ app/controllers/tbl_player_fees_controller.rb
@@ -2,7 +2,5 @@ class TblPlayerFeesController < ApplicationController
   layout 'admin'
   def index
     @playerfees = TblPlayerFee.all
-    @tblorders = TblOrder.all
-    @tblrooms = TblRoom.all
   end
 end
