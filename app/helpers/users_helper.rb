d08d05c44468c2c2b382e077f7fa2b1349adc93e
13949836783@163.com <13949836783@163.com>
2017-12-17 21:36:16 +0800

    完成提成算法

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -12,19 +12,37 @@ module UsersHelper
     money
   end
 
-# 二级代理算法
-  def render_salertwo_order(user)
+# 下线代理的代理发展玩家，提成算法
+  def render_saler_order(user)
+    sum = 0
+    @salertwo = User.where(:salerid => user.id)
+    result = []
+      @salertwo.each do |b|
+        c = User.where(:salerid => b.id)
+        result += c
+      end
+    @salerthree = result
+
+    @salerthree.each do |d|
+      r = User.find_by(:id => d.salerid)
+      sum += (render_total_order(d) * (user.rate - r.rate))
+    end
+    sum.round
+  end
+
+# 最终的提成总额
+  def render_order(user)
     sum = 0
     if user.permission == "三级代理"
       sum = (render_total_order(user) * user.rate)
     else
-      $u = User.where(:salerid => user.id )
+      $u = User.where(:salerid => user.id)
       $u.each do |i|
         sum += (render_total_order(i) * (user.rate - i.rate))
       end
     end
-    sum += render_total_order(user) * user.rate
-    sum
+    sum = sum + render_total_order(user) * user.rate + render_saler_order(user)
+    sum.round
   end
 
 end
