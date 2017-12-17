6ffaef21eb8553be2128854cee50881d255cfac9
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-12-17 15:55:23 +0800

    增添充值页面

--- app/controllers/tbl_player_fees_controller.rb
+++ app/controllers/tbl_player_fees_controller.rb
@@ -2,7 +2,7 @@ class TblPlayerFeesController < ApplicationController
   layout 'admin'
   def index
     @playerfees = TblPlayerFee.all
-    @tbl_orders = TblOrder.all
+    @tbl_orders = TblOrder.order("complete_time DESC")
     @tbl_room = TblRoom.all
   end
 end
