28f2cab8644dfd44729401cf34ee88bfbba3ba9f
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-12-17 16:03:27 +0800

    add paginate for playerfee

--- app/controllers/tbl_player_fees_controller.rb
+++ app/controllers/tbl_player_fees_controller.rb
@@ -1,8 +1,8 @@
 class TblPlayerFeesController < ApplicationController
   layout 'admin'
   def index
-    @playerfees = TblPlayerFee.all
-    @tbl_orders = TblOrder.order("complete_time DESC")
+    @playerfees = TblPlayerFee.page(params[:page]).per(5)
+    @tbl_orders = TblOrder.order("complete_time DESC").page(params[:page]).per(5)
     @tbl_room = TblRoom.all
   end
 end
