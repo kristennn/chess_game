e6b4a4488adc64729c151885a8c93c51a0fc2a91
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-12-17 22:27:36 +0800

    add order for playerfee

--- app/controllers/tbl_player_fees_controller.rb
+++ app/controllers/tbl_player_fees_controller.rb
@@ -1,7 +1,7 @@
 class TblPlayerFeesController < ApplicationController
   layout 'admin'
   def index
-    @playerfees = TblPlayerFee.page(params[:page]).per(5)
+    @playerfees = TblPlayerFee.order("userid DESC").page(params[:page]).per(5)
     @tbl_orders = TblOrder.order("complete_time DESC").page(params[:page]).per(5)
     @tbl_room = TblRoom.all
   end
