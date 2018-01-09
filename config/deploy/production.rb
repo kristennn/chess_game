98e9907700d2d6e10bafaa83496340b80ef2d4d2
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2018-01-09 13:17:00 +0800

    alter yml file and merge develop

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-set :branch, "develop"
+set :branch, "master"
 
   server "47.100.60.84", user: "jonathan", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
