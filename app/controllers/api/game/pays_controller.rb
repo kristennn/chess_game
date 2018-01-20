e2e2a60247e0bb9dd3eaf3f36ed7b02092dc324e
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2018-01-20 20:20:23 +0800

    debug

--- app/controllers/api/game/pays_controller.rb
+++ app/controllers/api/game/pays_controller.rb
@@ -2,17 +2,11 @@ class Api::Game::PaysController < ApiController
   def get_notify
     @diamond = params[:reserved1]
     @uid = params[:reserved2]
-    @money = params[:amount]
-    @tbl_account = TblAccount.find_by(:userid => @uid)
 
     require "net/http"
-    url = URI.parse('http://47.100.117.210:8691/update_data')
-    params = {:name = @diamond, :num = @amount, :uid = @uid}
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port){|http|
-      http.request(req)
-    }
-    puts res.body
+    url = URI.parse('http://47.100.117.210:8691/update_data?name=diamond&num=' +@diamond+ '&uid=' +@uid)
+    res = Net::HTTP.start(url.host, url.port)
+
 
     render :json => {
       :message => "ok"
@@ -24,4 +18,5 @@ class Api::Game::PaysController < ApiController
       :message => "支付成功"
     }
   end
+
 end
