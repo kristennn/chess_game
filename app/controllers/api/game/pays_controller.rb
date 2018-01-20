fb4e2031f394919d2ef404c87f34968ce80242a3
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2018-01-20 21:51:00 +0800

    插入order记录

--- app/controllers/api/game/pays_controller.rb
+++ app/controllers/api/game/pays_controller.rb
@@ -2,7 +2,16 @@ class Api::Game::PaysController < ApiController
   def get_notify
     @diamond = params[:reserved1]
     @uid = params[:reserved2]
+    @money = params[:amount]
+    @order_id = params[:orderid]
+    @datetime = params[:datetime]
 
+    @order = TblOrder.create( :userid   => params[:reserved2],
+                              :money    => params[:amount],
+                              :order_id => params[:orderid],
+                              :pay_time => params[:datetime],
+                            )
+    @order.save!
     require 'open-uri'
 
     uri = 'http://47.100.117.210:8691/update_data?name=diamond&num='+@diamond+'&uid='+@uid
--- app/models/tbl_account.rb
+++ app/models/tbl_account.rb
@@ -2,5 +2,5 @@ class TblAccount < ApplicationRecord
   self.table_name = 'tbl_account'
   belongs_to :player, class_name: "TblPlayerinfo", foreign_key: "userid"
   belongs_to :user, :class_name => "User", :foreign_key => "saler"
-  has_many :tbl_orders, :class_name => "TblOrder", :foreign_key => "userid"
+  # has_many :tbl_orders, :class_name => "TblOrder", :foreign_key => "userid"
 end
